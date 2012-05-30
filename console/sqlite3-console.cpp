f96ad7dd922b01ba74452603b80643b72c8ffdc6
wush <wush@f2ware.com>
2012-05-30 18:44:17 +0800

    create table

--- console/sqlite3-console.cpp
+++ console/sqlite3-console.cpp
@@ -11,9 +11,18 @@ int main(int argc, char* argv[]) {
 	try {
 		using namespace SQLite3;
 		Db db(argv[1]);
-
+		std::vector<Db::ColumnDef> column_def;
+		column_def.resize(3);
+		column_def[0].column_label.assign("test0");
+		column_def[0].column_type = Db::Integer;
+		column_def[1].column_label.assign("test1");
+		column_def[1].column_type = Db::Text;
+		column_def[2].column_label.assign("test2");
+		column_def[2].column_type = Db::Blob;
+		db.createTable("test_table", column_def, false);
 	}
 	catch (SQLite3::exception &e) {
+		std::cout << "An exception occurred:" << std::endl;
 		std::cout << e.what() << std::endl;
 		return 1;
 	}
